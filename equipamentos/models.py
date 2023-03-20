57f45a945563795883b59df4c478ccb894b497be
higorbreno <higorbreno02@gmail.com>
2023-03-19 22:54:20 -0300

    mais mudanças

--- equipamentos/models.py
+++ equipamentos/models.py
@@ -1,10 +1,4 @@
 from django.db import models
-from autoslug import AutoSlugField
-
-MESES = {'Janeiro', 'Fevereiro', 'Março', 'Abril', 
-         'Maio', 'Junho', 'Julho', 'Agosto', 
-         'Setembro', 'Outubro', 'Novembro', 'Dezembro'}
-MESES_CHOICES = ([item[1][0], item[0]] for item in MESES)
 
 # Create your models here.
 class Equipamento(models.Model):
@@ -20,7 +14,7 @@ class Equipamento(models.Model):
 
 class Mes(models.Model):
     created = models.DateField(auto_now_add=True)
-    title = models.CharField(choices=MESES_CHOICES, default='Janeiro', max_length=100)
+    title = models.TextField()
     mes = models.DateField()
 
     class Meta:
